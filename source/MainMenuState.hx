32790e4a7e528b27e4d16ca7ccdb089122514550
travisgamemaker <woodrufftravis196@gmail.com>
2022-05-23 15:51:44 -0500

    i will make you will break at the hands of a god

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -25,7 +25,7 @@ using StringTools;
 
 class MainMenuState extends MusicBeatState
 {
-	public static var psychEngineVersion:String = 'TGMix 0.1.5h'; //This is also used for Discord RPC
+	public static var psychEngineVersion:String = 'TGMix 0.1.5'; //This is also used for Discord RPC
 	public static var curSelected:Int = 0;
 
 	var menuItems:FlxTypedGroup<FlxSprite>;
